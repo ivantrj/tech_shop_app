aeb5342f3e0f2544e122ee390e6ff1bbdd125c7e
ivan <ivantrajanovski@gmail.com>
2023-11-14 13:53:28 +0100

    first commit

---
+++ TechShopApp/TechShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechShopAppApp.swift
+//  TechShopApp
+//
+//  Created by ivan ruwido  on 14.11.23.
+//
+
+import SwiftUI
+
+@main
+struct TechShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
